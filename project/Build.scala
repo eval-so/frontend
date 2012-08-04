e28503273e1181b820429790791013df2b5f7d08
Ricky Elrod <ricky@elrod.me>
2012-08-04 13:32:20 -0400

    Revert "Try using firefox for tests?"
    This reverts commit 64976ce44a8c796eef1462d5c531e361d719dd14.

--- project/Build.scala
+++ project/Build.scala
@@ -10,8 +10,7 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
       "postgresql" % "postgresql" % "9.1-901.jdbc4",
       "joda-time" % "joda-time" % "2.1",
-      "javax.mail" % "mail" % "1.4.5",
-      "org.seleniumhq.selenium" % "selenium-firefox-driver" % "2.24.1" % "test"
+      "javax.mail" % "mail" % "1.4.5"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
--- test/IntegrationTests.scala
+++ test/IntegrationTests.scala
@@ -8,7 +8,7 @@ import play.api.test.Helpers._
 class IntegrationTest extends Specification {
   "Frontend" should {
     "work from within a browser" in {
-      running(TestServer(3333), FIREFOX) { browser =>
+      running(TestServer(3333), HTMLUNIT) { browser =>
         browser.goTo("http://localhost:3333/")
 
         // Able to load the front page

d7189994a7373004b52590376bd6e1185d4a05b3
Ricky Elrod <ricky@elrod.me>
2012-09-24 00:54:27 -0400

    The start of a profile page, of sorts.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,13 +12,26 @@ import models.{Server}
   */
 object Application extends Controller with Auth with AuthConfigImpl {
 
-  /** Generate a sha256 hash from a given string
+  /** Generate a sha256 hash from a given string.
     *
     * @param s the String to hash.
     * @return a String which is the sha256sum result.
     */
    def sha256(s: String): String = {
      val m = java.security.MessageDigest.getInstance("SHA-256")
+     val b = s.getBytes("UTF-8")
+     m.update(b, 0, b.length)
+     new java.math.BigInteger(1, m.digest).toString(16)
+   }
+
+  /** Generate an md5sum hash from a given string.
+    *
+    * @param s the String to hash.
+    * @todo combine this with sha256 above and rename to something generalized.
+    * @return a String which is the sha256sum result.
+    */
+   def md5(s: String): String = {
+     val m = java.security.MessageDigest.getInstance("MD5")
      val b = s.getBytes("UTF-8")
      m.update(b, 0, b.length)
      new java.math.BigInteger(1, m.digest).toString(16)
@@ -31,7 +44,7 @@ object Application extends Controller with Auth with AuthConfigImpl {
   def index = optionalUserAction { user => implicit request =>
     user match {
       case None => Ok(views.html.index(user, UserController.registerForm))
-      case Some(user) => Redirect(routes.BreakpointApplicationController.myApplications)
+      case Some(user) => Ok(views.html.user.profile(user))
     }
   }
 

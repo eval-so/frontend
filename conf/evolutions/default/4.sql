380d3ca6d30e4bf7edcfbc69484f6de6a5351abe
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2012-10-10 06:00:04 -0400

    Fix evolution #4 to be conforming SQL.
    Set default values on new, not null columns, or else there could be
    very bad things happening (like failed evolutions).

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -1,9 +1,9 @@
 # ---!Ups
 drop index application_users_idx;
 alter table application_users drop column secret_key;
-alter table users add column secret_key varchar(36) not null;
+alter table users add column secret_key varchar(36) not null default '';
 create index users_secret_key_idx on users (username, secret_key);
-alter table applications add column allow_anonymous_evals boolean not null;
+alter table applications add column allow_anonymous_evals boolean not null default false;
 
 # ---!Downs
 alter table application_users add column secret_key varchar(36) not null;

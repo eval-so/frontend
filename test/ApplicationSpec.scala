4902fd42bc67278a849f9e8436766040e172cbcb
Ricky Elrod <ricky@elrod.me>
2013-04-21 15:57:46 -0400

    Add some specs for when /api/evaluate should return BadRequest

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -5,6 +5,7 @@ import org.scalatest.matchers.ShouldMatchers
 
 import play.api.test._
 import play.api.test.Helpers._
+import play.api.libs.json._
 
 class ApplicationSpec
   extends FunSpec
@@ -13,7 +14,7 @@ class ApplicationSpec
   with BeforeAndAfter
   with ParallelTestExecution {
 
-  describe("The Application controller") {
+  describe("Frontend") {
     it("should send 404 on a bad request") {
       running(FakeApplication()) {
         route(FakeRequest(GET, "/boum")) should be (None)
@@ -28,7 +29,9 @@ class ApplicationSpec
         contentAsString(home) should include ("Welcome to eval.so")
       }
     }
+  }
 
+  describe("The languages list") {
     it("should return JSON containing the available languages") {
       running(FakeApplication()) {
         val languages = route(FakeRequest(GET, "/api/languages")).get
@@ -36,6 +39,22 @@ class ApplicationSpec
         contentType(languages) should be (Some("application/json"))
         contentAsString(languages) should include (""""languages":""")
         contentAsString(languages) should include ("""ruby""")
+      }
+    }
+  }
+
+  describe("The code evaluation endpoint") {
+    it("should return BadRequest when missing required arguments") {
+      running(FakeApplication()) {
+        val missingCode = Json.obj("language" -> "ruby")
+        val missingCodeEval = route(FakeRequest(POST, "/api/evaluate").withJsonBody(missingCode)).get
+        status(missingCodeEval) should be (BAD_REQUEST)
+        contentType(missingCodeEval) should be (Some("application/json"))
+
+        val missingLanguage = Json.obj("code" -> "puts 123")
+        val missingLanguageEval = route(FakeRequest(POST, "/api/evaluate").withJsonBody(missingLanguage)).get
+        status(missingLanguageEval) should be (BAD_REQUEST)
+        contentType(missingLanguageEval) should be (Some("application/json"))
       }
     }
   }
